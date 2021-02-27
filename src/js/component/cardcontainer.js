e1088784f0d657b4f4d33e6f5b4cd24770663529
jairosantamaria <jairosantamaria@amelearning.net>
2021-02-27 03:49:59 +0000

    tercer rev

--- src/js/component/cardcontainer.js
+++ src/js/component/cardcontainer.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 import card from "./card";
 
 export const CardContainer = () => {
-    return (
-        <div classname="row">
-            <div className="col-3"><Card /></div>
-            <div className="col-3"><Card /></div>
-            <div className="col-3"><Card /></div>
-            <div className="col-3"><Card /></div>
-        </div>
-    );
-}
+	return (
+		<div className="row">
+			<div className="col-3">
+				<Card />
+			</div>
+			<div className="col-3">
+				<Card />
+			</div>
+			<div className="col-3">
+				<Card />
+			</div>
+			<div className="col-3">
+				<Card />
+			</div>
+		</div>
+	);
+};
 export default CardContainer;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 //include images into your bundle
-import Navbar from "./Navbar";
-import CardContainer from "./cardcontainer";
+import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
+import CardContainer from "./cardcontainer";
 
 //create your first component
 export function Home() {
 	return (
 		<div>
 			<Navbar />
-             <div className="container">
-                 <Jumbotron />
-             </div>
+			<div className="container">
+				<Jumbotron />
+			</div>
 		</div>
 	);
 }
